16d4d3914fb9dfd42594b569ec332abec8a6507f
Eric Johnson <ejohns32@calpoly.edu>
2014-03-04 23:46:15 -0800

    made swarm spawning parallel and made it random

--- swarmDriver.cu
+++ swarmDriver.cu
@@ -1,25 +1,49 @@
 #include <unistd.h>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
+#include <thrust/tabulate.h>
+#include <math_constants.h>
 
 #include "swarmAgent.h"
 #include "swarmQuad.h"
 #include "swarmGraphics.h"
 
 const int NUM_TEAMS = 2;
-const int NUM_AGENTS_PER_TEAM = 20;
+const int NUM_AGENTS_PER_TEAM = 32;
+
+
+// from fun_with_points
+__host__ __device__
+float hashRand(unsigned int x)
+{
+	x = (x+0x7ed55d16) + (x<<12);
+	x = (x^0xc761c23c) ^ (x>>19);
+	x = (x+0x165667b1) + (x<<5);
+	x = (x+0xd3a2646c) ^ (x<<9);
+	x = (x+0xfd7046c5) + (x<<3);
+	x = (x^0xb55a4f09) ^ (x>>16);
+	return float(x) / UINT_MAX;
+}
+
+struct SpawnTeam {
+	uint8_t numTeams;
+	uint16_t numPerTeam;
+	uint32_t total;
+
+	SpawnTeam(uint8_t numTeams, uint16_t numPerTeam) : numTeams(numTeams), numPerTeam(numPerTeam), total(numTeams * numPerTeam) {}
+
+	__host__ __device__ SwarmAgent operator()(unsigned int i) {
+		uint8_t team = i / numPerTeam;
+
+		return SwarmAgent(team, (hashRand(i) + team) * SwarmAgent::maxPosition().x / numTeams, hashRand(i + total) * SwarmAgent::maxPosition().y, cos(2 * CUDART_PI_F * hashRand(i + 2 * total)), sin(2 * CUDART_PI_F * hashRand(i + 2 * total)));
+	}
+};
 
 void setup(thrust::device_vector<SwarmAgent> &dSwarm)
 {
 	thrust::host_vector<SwarmAgent> hSwarm(NUM_TEAMS * NUM_AGENTS_PER_TEAM);
 
-	for (int team = 0; team < NUM_TEAMS; ++team)
-	{
-		for (int agent = 0; agent < NUM_AGENTS_PER_TEAM; ++agent)
-		{
-			hSwarm[team * NUM_AGENTS_PER_TEAM + agent] = SwarmAgent(team, agent * 2.0f, team * 3.0f, 2.0f, 1.0f);
-		}
-	}
+	thrust::tabulate(hSwarm.begin(), hSwarm.end(), SpawnTeam(NUM_TEAMS, NUM_AGENTS_PER_TEAM));
 
 	dSwarm = hSwarm;
 }
